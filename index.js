a5843af45b976a220970f3914c3cb0129889d027
Emanuel <gilrestrepoemanuel@gmail.com>
2024-10-05 09:48:10 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("main_div");
+button.addEventListener("click" , function(){
+    alert("hola mundo");
+})
+
+// let main_div = document.querySelector(".main_div");
